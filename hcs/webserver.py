4c38e49c25a7228717014b49cbaab409c7f77304
Chethaka Sathmal <csathmal24@gmail.com>
2025-12-01 16:56:43 +0530

    Finished initial webserver.py

--- hcs/webserver.py
+++ hcs/webserver.py
@@ -7,26 +7,52 @@
 6. Handle new connections
 """
 
+"""
+* Include:
+    * How to queue multiple requests
+    * How to handle multiple requests
+        * Sequentially 
+        * Asynchronously 
+"""
+
 import socket
 import sys
 
 HOST: str = "127.0.0.1"
 PORT: int = 8080
-
+BUFFER: int = 4096
+ENCODING: str = "ISO 8859-1"
 
 def main():
     res: str = (
-        "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 16\nConnection: close\r\n\r\nHello"
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 16\r\nConnection: close\r\n\r\nHello"
     )
 
     try:
         listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 6) or None
         listen_sock.bind((HOST, PORT))
+        listen_sock.listen()
+
+        while True:
+            client_con = listen_sock.accept()
+            client_sock = client_con[0]
+
+            req: bytes = b""
+            while True:
+                data = client_sock.recv(BUFFER)
+                req += data
+                if b"\r\n\r\n" in req:
+                    break
+
+            print(req.decode(ENCODING))
+
+            client_sock.send(res.encode(ENCODING))
+            client_sock.close()
     except Exception as e:
         print(f"Error: {e}")
     finally: 
-        if sock:
-            sock.close()
+        if listen_sock:
+            listen_sock.close()
 
 
 if __name__ == "__main__":
